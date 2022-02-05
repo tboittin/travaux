a86a18b31d6c87f50a25606b45ffb9b18dcf054b
Thomas Boittin <tboittin@gmail.com>
2022-02-05 15:15:42 +0100

    retrieve data

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,8 +1,18 @@
-const NewsList = () => {
+import useDataFetcher from "../hooks/dataFetcher"
 
+const NewsList = () => {
+  const type = "top" //todo pass it as parameter
+  const { isLoading, stories } = useDataFetcher(type ? type : "top")
+  console.log(stories)
   return (
     <div>
-      NewsList
+      <h2>News List</h2>
+      <ul>
+        {!isLoading &&
+          stories.map(
+            ({ data: story }) => story && <li key={story.id}>{story.id}</li>
+          )}
+      </ul>
     </div>
   )
 }
